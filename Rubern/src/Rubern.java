9c919bb3df24c479b1d111a41f63ae9dcd297390
Matias Venditti <Matias Venditti>
2016-10-11 16:02:31 -0300

    calculo de costo de imagen en Rubern

--- Rubern/src/Rubern.java
+++ Rubern/src/Rubern.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Matias on 11/10/2016.
@@ -8,13 +10,20 @@ public class Rubern {
     private ArrayList<Chofer> choferes = new ArrayList<>();
     private ArrayList<Cliente> clientes = new ArrayList<>();
     private double distanciaMinima;
+    private Map<Chofer, Double> costosDeImagen = new HashMap<>();
 
     public void iniciarViaje(Solicitud solicitud){
+        // Por cada chofer disponible, y dentro de la distancia minima, se calcula el costo de imagen y se añade a un hashmap que
+        // contiene choferes con sus respectivos costos de imagen para despues ordenar dicho mapa respecto de los costos de imagen asi
+        // poder obtener el chofer cuyo costo sea menor.
         for(Chofer c: choferes){
-            if (c.estaDisponible(solicitud.getFecha())){  //AGREGAR EL TEMA DE QUE TIENEN QUE ESTAR DENTRO DE LA DISTANCIA MINIMA
+            if (c.estaDisponible(solicitud.getFecha()) && (c.getAuto().getCoordenadasAuto().distance(solicitud.getInicio()) <= distanciaMinima)){
                 calcularCantidadAutos(solicitud.getPasajeros(),c);
-
+                costosDeImagen.put(c,calcularCantidadAutos(solicitud.getPasajeros(),c)*((c.getAuto().getCoordenadasAuto().distance(solicitud.getInicio()))/250));
             }
+        }
+        for (Map.Entry<Chofer, Double> entry: costosDeImagen.entrySet()){
+            //OBTENER EL MENOR COSTO DE IMAGEN.
         }
     }
 
