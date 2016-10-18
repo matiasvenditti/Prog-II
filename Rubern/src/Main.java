9fb60d863f7d8dd13d24392ce2b5fff446132720
Matias Venditti <Matias Venditti>
2016-10-18 09:18:37 -0300

    agrego directorio out

--- Rubern/src/Main.java
+++ Rubern/src/Main.java
@@ -5,41 +5,41 @@ import javax.swing.*;
  */
 public class Main {
 
-    public static void main (String[] args){
-
-        //Inicializacion de una persona para la prueba;
-        TarjetaCredito tarjeta1 = new TarjetaCredito("777", 1000);
-        Point2D coordenadasNaiara = new Point2D(1,2);
-        Cliente naiara = new Cliente("Naiara", "+34644444397", "naiara@naiara.com", tarjeta1, coordenadasNaiara, 100);
-
-        //Inicializacion de dos chófer para la prueba;
-        Auto auto1=new Auto("BMW", "familiar", "negro", "3703BFX",6);
-        Auto auto2=new Auto("Mercedes", "deportivo", "rojo", "1256BHB",4);
-        Interval jornada1= new Interval();
-        TarjetaCredito tarjeta2=new TarjetaCredito("444", 1000);
-        TarjetaCredito tarjeta3=new TarjetaCredito("333", 1000);
-        Chofer Juan= new Chofer(auto1, jornada1, "Juan", "+34644444444", "juan@juan.com", tarjeta2);
-        Chofer Iker= new Chofer(auto2, jornada1, "Iker", "+34677777777", "iker@iker.com", tarjeta3);
-
-        int operacion;
-        operacion = Integer.parseInt(JOptionPane.showInputDialog("rUberN\n1. Solicitar viaje.\n2.Consultar viajes."));
-        switch(operacion){
-            case 1:
-                double numPersonas = Integer.parseInt(JOptionPane.showInputDialog("Inserte número de viajeros: "));
-                double puntoOrigenX = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada X del origen: "));
-                double puntoOrigenY = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada Y del origen: "));
-                double puntoDestinoX = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada X del destino: "));
-                double puntoDestinoY = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada Y del destino: "));;
-                Point2D puntoOrigen = new Point2D(puntoOrigenX,puntoOrigenY);
-                Point2D puntoDestino;
-                puntoOrigen = (2,2);
-                puntoOrigen.setLocation(puntoOrigenX, puntoOrigenY);
-                puntoDestino.setLocation(puntoOrigenX, puntoOrigenY);
-                Solicitud solicitud=new Solicitud(numPersonas, puntoOrigen, puntoDestino, fecha);
-
-
-            case 2:
-
-        }
-    }
+//    public static void main (String[] args){
+//
+//        //Inicializacion de una persona para la prueba;
+//        TarjetaCredito tarjeta1 = new TarjetaCredito("777", 1000);
+//        Point2D coordenadasNaiara = new Point2D(1,2);
+//        Cliente naiara = new Cliente("Naiara", "+34644444397", "naiara@naiara.com", tarjeta1, coordenadasNaiara, 100);
+//
+//        //Inicializacion de dos chófer para la prueba;
+//        Auto auto1=new Auto("BMW", "familiar", "negro", "3703BFX",6);
+//        Auto auto2=new Auto("Mercedes", "deportivo", "rojo", "1256BHB",4);
+//        Interval jornada1= new Interval();
+//        TarjetaCredito tarjeta2=new TarjetaCredito("444", 1000);
+//        TarjetaCredito tarjeta3=new TarjetaCredito("333", 1000);
+//        Chofer Juan= new Chofer(auto1, jornada1, "Juan", "+34644444444", "juan@juan.com", tarjeta2);
+//        Chofer Iker= new Chofer(auto2, jornada1, "Iker", "+34677777777", "iker@iker.com", tarjeta3);
+//
+//        int operacion;
+//        operacion = Integer.parseInt(JOptionPane.showInputDialog("rUberN\n1. Solicitar viaje.\n2.Consultar viajes."));
+//        switch(operacion){
+////            case 1:
+////                double numPersonas = Integer.parseInt(JOptionPane.showInputDialog("Inserte número de viajeros: "));
+////                double puntoOrigenX = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada X del origen: "));
+////                double puntoOrigenY = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada Y del origen: "));
+////                double puntoDestinoX = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada X del destino: "));
+////                double puntoDestinoY = Double.parseDouble(JOptionPane.showInputDialog("Inserte coordenada Y del destino: "));;
+////                Point2D puntoOrigen = new Point2D(puntoOrigenX,puntoOrigenY);
+////                Point2D puntoDestino;
+////                puntoOrigen = (2,2);
+////                puntoOrigen.setLocation(puntoOrigenX, puntoOrigenY);
+////                puntoDestino.setLocation(puntoOrigenX, puntoOrigenY);
+////                Solicitud solicitud=new Solicitud(numPersonas, puntoOrigen, puntoDestino, fecha);
+//
+//
+//            case 2:
+//
+//        }
+//    }
 }
