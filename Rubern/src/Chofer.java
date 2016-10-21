3041a129f736449cdf9966c0a2a9ff7cafa8a6c6
Matias Venditti <Matias Venditti>
2016-10-21 12:07:36 -0300

    agrego metodo viajar en chofer

--- Rubern/src/Chofer.java
+++ Rubern/src/Chofer.java
@@ -29,23 +29,31 @@ public class Chofer extends Persona{
 
     public void enviarViaje(Solicitud solicitud){
         //Le asigno un 50% de chances de que el chofer acepte o no el viaje con un numero random.
+        // si el cliente no puede pagar el viaje o el chofer lo rechaza, no se realiza el viaje.
         Random random = new Random();
         int number = random.nextInt(2);
-        double distance = Math.sqrt(Math.pow(solicitud.getFin().getxPosition()-solicitud.getInicio().getxPosition(),2)+(Math.pow(solicitud.getFin().getyPosition()-solicitud.getInicio().getyPosition(),2)));
-        costo = 15 + (distancia/100);
+
         if (number == 0 && solicitud.getCliente().getTarjetaCredito().getSaldo() > costo){
             System.out.println("El chofer acepto el viaje: " + solicitud.toString());
 
             System.out.println("Con un costo de: " + distancia);
-            aceptaViaje = true;
-
-            solicitud.getCliente().pagarViaje(costo, solicitud, this);
+            viajar(solicitud);
         }
         else{
             System.out.println("El chofer no acepto el viaje: " + solicitud.toString());
             aceptaViaje = false;
         }
 
+    }
+
+    public void viajar(Solicitud solicitud){
+        //acepta el viaje, no esta mas disponible, se actualizan las coordenadas del auto, y se cobra al cliente.
+        double distancia = Math.sqrt(Math.pow(solicitud.getFin().getxPosition()-solicitud.getInicio().getxPosition(),2)+(Math.pow(solicitud.getFin().getyPosition()-solicitud.getInicio().getyPosition(),2)));
+        costo = 15 + (distancia/100);
+        aceptaViaje = true;
+        disponible = false;
+        this.getAuto().actualizarCoordenadas(solicitud.getFin());
+        solicitud.getCliente().pagarViaje(costo,solicitud,this);
     }
 
     public boolean isAceptaViaje() {
