111dc8e8ce58e3aa2c7189b307b761102871427b
Matias Venditti <Matias Venditti>
2016-10-28 11:03:27 -0300

    Auto

--- Rubern/src/Auto.java
+++ Rubern/src/Auto.java
@@ -1,41 +1,23 @@
+import java.awt.geom.*;
 
 /**
  * Created by Matias on 11/10/2016.
  */
 public class Auto {
 
-    private String marca;
-    private String modelo;
-    private String color;
-    private String matricula;
+
+
+
+
     private int capacidadMax;
     private Point2D coordenadasAuto;
 
-    public Auto(String unaMarca, String unModelo, String unColor, String unaMatricula, int unaCapacidadMaxima){
+    public Auto(int unaCapacidadMaxima, Point2D coordenadasAuto){
 
-        marca=unaMarca;
-        modelo=unModelo;
-        color=unColor;
-        matricula=unaMatricula;
         capacidadMax=unaCapacidadMaxima;
+        this.coordenadasAuto = coordenadasAuto;
     }
 
-
-    public String getMarca(){
-        return marca;
-    }
-
-    public String getMoedelo(){
-        return modelo;
-    }
-
-    public String getColor(){
-        return color;
-    }
-
-    public String getMatricula(){
-        return matricula;
-    }
 
     public int getCapacidadMax(){
         return capacidadMax;
